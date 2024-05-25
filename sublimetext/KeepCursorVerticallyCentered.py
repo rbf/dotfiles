46b87778d310528d01f23166f7dda968104fcc06
rbf <rbf.code@pm.me>
2024-05-25 23:36:23 +0200

    new: [sublimetext] enhance plugin to vertically center current line

--- sublimetext/KeepCursorVerticallyCentered.py
+++ sublimetext/KeepCursorVerticallyCentered.py
@@ -6,15 +6,38 @@ import sublime_plugin
 # `'keep_cursor_vertically_centered': true` to the file
 # [usual path]/Packages/User/Distraction Free.sublime-settings
 
+# SOURCE: https://stackoverflow.com/a/47786967
+# SOURCE: https://forum.sublimetext.com/t/always-centered-cursor/4005/3
+
+def center_cursor_vertically(view):
+    sel = view.sel()
+    if len(sel) == 1:
+        # Use .b to keep the cursor centered when selecting lines with SHIFT and
+        # the cursor keys.
+        view.show_at_center(sel[0].b, animate = True)
+
+
 class KeepCursorVerticallyCenteredCommand(sublime_plugin.EventListener):
-    # SOURCE: https://stackoverflow.com/a/47786967
-    # SOURCE: https://forum.sublimetext.com/t/always-centered-cursor/4005/2
-    # SOURCE: https://forum.sublimetext.com/t/always-centered-cursor/4005/3
-    # def on_modified(self, view):
+    def __init__(self):
+        self.allow_scroll_view = False
+
+    def on_text_command(self, view, command_name, args):
+        # Do not scroll the view if clicking with the mouse, i.e. when the
+        # command "drag_select" is fired, as the behavior feels too erratic.
+        self.allow_scroll_view = command_name != "drag_select"
+
+    def on_modified(self, view):
+        self.allow_scroll_view = True
+
     def on_selection_modified(self, view):
-        if view.settings().get('keep_cursor_vertically_centered'):
-            print("yay")
-            sel = view.sel()
-            pt = sel[0].begin() if len(sel) == 1 else None
-            if pt != None:
-                view.show_at_center(pt)
+        if self.allow_scroll_view and view.settings().get('keep_cursor_vertically_centered'):
+            center_cursor_vertically(view)
+
+
+class ToggleViewKeepCursorVerticallyCenteredCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        settings = self.view.settings()
+        new_value = not settings.get("keep_cursor_vertically_centered", False)
+        settings.set("keep_cursor_vertically_centered", new_value)
+        if new_value:
+            center_cursor_vertically(self.view)
